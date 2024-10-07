3fa8dd323ed25cc0aa0d1056b3589a8efd792c19
Arghyrupa Bhanjdeo <arghyarupabhanjdeo@gmail.com>
2024-10-07 12:48:02 +0530

    commit

---
+++ Frontend/Javascript/Class1/index.js
@@ -0,0 +1,94 @@
+// console.log("hey");
+// var name = "dik";
+// let age = 22;
+// const city = "bbsr";
+// console.log(age);
+
+// console.log(address);
+
+// var address = "Bhubaneswar";
+// console.log(address);
+
+//Null
+// let user;
+// let user = null;
+// var user = null;
+// const user=null;
+// console.log(typeof null);
+// console.log(user);
+
+//Undefined
+// let data;
+// console.log(typeof data);
+// console.log(data);
+
+//Number
+// let num1 = 50;
+// let decimal_no = 1.001;
+// let pi=3.14;
+// let age=25;
+// let rollno=21;
+// console.log(typeof num1);
+
+//Addition
+// let num2 = 100;
+// let num3 = 125;
+// let addition = num2 + num3;
+// let sub = num2 - num3;
+// let mul = num2 * num3;
+// let div = num2 % num3;
+// let mod = num2 / num3;
+// console.log(addition);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+// console.log(mod);
+
+//Bignit
+// let huge_no=34567666666666;
+// console.log(typeof huge_no);
+
+//symbol
+// let sim1=Symbol("sim1");
+// let sim2=Symbol("sim2");
+// console.log(sim1==sim2)  //false
+
+//Nan
+// let num5 = 0;
+// let num6 = 0;
+// console.log(num5 / num6);
+// let num1 = 0;
+// let num2 = 10;
+// console.log(num1 / num2);
+// console.log(num2 / num1);
+
+//Boolean
+// let isMarried = false;
+// let isSingle = true;
+// console.log(typeof isMarried);
+// console.log(typeof isSingle);
+
+//String
+
+//Template literal:
+let name = "dikshya";
+let age = 22;
+let city = "KJR";
+let rollno = 16;
+let college = "usbm";
+let batch = "MCA";
+// console.log(`my name is ${name}`);
+// console.log(`my age is ${age}`);
+// console.log(`my Rollno is ${rollno}`);
+// console.log(`my city is ${city}`);
+// console.log(`my college is ${college}`);
+// console.log(`my batch is ${batch}`);
+console.log(
+  `My name is ${name},  From ${city}, and age is ${age} , I am currently study on ${batch} in the ${college} college and my rollnumber is ${rollno}`
+);
+
+//To Check Length Of the String
+// let city = "BBSR";
+// console.log(city.length);
+
+//Replace
---
+++ Frontend/Javascript/Class1/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let name = "dikshya";
+// let age = 22;
+// let city = "KJR";
+// let rollno = 16;
+// let college = "usbm";
+// let batch = "MCA";
